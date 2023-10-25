9d45e132f04b30f0f76bc0ee1315538ff4ca3df7
Sudhamsh Mondrati <sudhamshmondrati@gmail.com>
2023-10-25 19:04:08 -0400

    Add on-click handlers for buttons

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,23 @@
+const handleClick = function () {
+  console.log("You clicked it!");
+};
+
+const handleAnonymousClick = function (name) {
+  console.log(`Hello to you, ${name}!`);
+};
+
 const Home = () => {
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me!</button>
+      <button
+        onClick={function () {
+          handleAnonymousClick("Zoom");
+        }}
+      >
+        Anonymous click!
+      </button>
     </div>
   );
 };
