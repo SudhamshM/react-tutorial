cb4df2cf7b11d014ea2c1b91ed51ed4eeb21eb04
Sudhamsh Mondrati <sudhamshmondrati@gmail.com>
2023-10-25 18:31:35 -0400

    Add other components for app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
+  const title = "Welcome to Sudhamsh's blog!";
+  const likes = 49;
+  const link = "https://www.google.com/";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="content">
+        <Home />
+        {/* <h1>{title}</h1>
+        <p>Liked {likes} times.</p>
+        <p>{45}</p>
+        <p>{[1, 2, 54, 6]}</p>
+        <a href={link}>Google</a>
+        <h2>App Component</h2> */}
+      </div>
     </div>
   );
 }
