9d15c1d4574c2fbb6a54acfcd29d9b23fc7ca3f7
Sudhamsh Mondrati <sudhamshmondrati@gmail.com>
2023-10-25 19:31:37 -0400

    Use hooks for state and see how component REACTs

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,26 @@
+import { useState } from "react";
+
 const Home = () => {
-  let name = "Godspeed";
+  // let name = "Godspeed";
+  const [name, setName] = useState("August Heart");
+  const [age, setAge] = useState(34);
   console.log("Home component started, name value: ", name);
+
   const handleClick = function (event) {
     console.log("Entered handleClick function...");
-    name = "Zoom";
+
+    setName("Hunter Zolomon");
+    setAge(38);
+
     console.log("Clicked, name value: ", name);
   };
 
   return (
     <div className="home">
       <h2>Homepage</h2>
-      <p>{name}</p>
+      <p>
+        {name} is {age} years old.
+      </p>
       <button onClick={handleClick}>Click me!</button>
     </div>
   );
