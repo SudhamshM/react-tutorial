332309ef83b8267e317bed4073491818fa1a1cd8
Sudhamsh Mondrati <sudhamshmondrati@gmail.com>
2023-10-25 20:21:46 -0400

    Update styling for delete button - lesson 13

--- src/BlogList.js
+++ src/BlogList.js
@@ -10,10 +10,22 @@ const BlogList = function ({ blogs, title, deleteHandler }) {
         <h2>{title}</h2>
 
         {blogs.map((blog) => (
-          <div className="blog-preview" key={blog.id}>
+          <div
+            className="blog-preview"
+            key={blog.id}
+            style={{ position: "relative" }}
+          >
             <h2>{blog.title}</h2>
             <p>Written by {blog.author}</p>
             <button
+              className="createBtn"
+              style={{
+                position: "absolute",
+                border: "none",
+                top: "20%",
+                right: "5%",
+                cursor: "pointer",
+              }}
               onClick={function () {
                 deleteHandler(blog.id);
               }}
