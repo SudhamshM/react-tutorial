3275afc564c0743a21d0b4a0409e3fbdf9c29d39
Sudhamsh Mondrati <sudhamshmondrati@gmail.com>
2023-11-15 18:08:28 -0500

    Add error handling for fetch in React

--- src/Home.js
+++ src/Home.js
@@ -4,24 +4,39 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
 
-  const [name, setName] = useState('mario')
+  const [isPending, setIsPending] = useState(true)
+  const [error, setError] = useState(null)
 
   // useEffect function runs EVERY TIME there is a render of the component,
   // such as whenever the state changes and causes a render, useEffect runs
   useEffect(function () {
     console.log("Entered useEffect function...")
-    fetch("http://localhost:8000/blogs")
-      .then(res => {
-        return res.json()
-      })
-      .then(data => {
-        console.log("Data: ", data);
-        setBlogs(data)
-      })
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogss")
+        .then(res => {
+          if (!res.ok) {
+            throw Error("Could not fetch for that resource.")
+          }
+          return res.json()
+        })
+        .then(data => {
+          console.log("Data: ", data);
+          setBlogs(data);
+          setIsPending(false)
+          setError(null)
+        })
+        .catch((e) => {
+          setError(e.message);
+          setIsPending(false)
+        })
+    }, 1000);
+
   }, [])
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
